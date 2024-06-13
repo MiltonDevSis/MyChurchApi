f703d82240bb28d5b888a76f9dd565a1f91ca252
Milton <milton.dev.sis@gmail.com>
2024-06-13 10:43:06 -0300

    Criado modelo, mapeamento e migration para NaturezaDeLancamento

--- MyChurch.Api/Data/ApplicationContext.cs
+++ MyChurch.Api/Data/ApplicationContext.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MyChurch.Api.Data.Mappings;
 using MyChurch.Api.Domain.Models;
@@ -12,11 +8,14 @@ namespace MyChurch.Api.Data
     {
         public DbSet<Usuario> Usuario { get; set; }
 
+        public DbSet<NaturezaDeLancamento> NaturezaDeLancamento { get; set; }
+
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new NaturezaDeLancamentoMap());
         }
     }
 }
--- MyChurch.Api/Migrations/20240613133534_CriarEntidadeNaturezaDeLancamento.Designer.cs
+++ MyChurch.Api/Migrations/20240613133534_CriarEntidadeNaturezaDeLancamento.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyChurch.Api.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MyChurch.Api.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240613133534_CriarEntidadeNaturezaDeLancamento")]
+    partial class CriarEntidadeNaturezaDeLancamento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
